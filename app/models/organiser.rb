a637a024af5b843e327a3f9e361fb9fb41d7f047
Lukasz Jankowski <lukaszpjankowski@gmail.com>
2015-01-26 22:32:14 +0100

    one line

--- app/models/organiser.rb
+++ app/models/organiser.rb
@@ -37,7 +37,7 @@ scope :sorted_by, lambda { |sort_option|
   	where(id: country_ids)
   }
   scope :with_created_at_gte, lambda { |ref_date|
-  	where(id: ref_date)
+  	where(created_at: ref_date)
   }
 
 scope :search_query, lambda { |query|
